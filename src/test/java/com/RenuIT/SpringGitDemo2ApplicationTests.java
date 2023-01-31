f3dc8c73c4de601d411aa90016bc3979033c7cbb
PKiran2548 <kiranpaithane@gmail.com>
2023-01-31 13:51:41 +0530

    changes made by Pkiran

---
+++ src/test/java/com/RenuIT/SpringGitDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RenuIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
